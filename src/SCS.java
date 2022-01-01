047fee1f08867b8602ad00431152328bf36ef327
Suleimen <lmfao159753@mail.ru>
2022-01-01 18:31:12 +0500

    battery spec added, user menu added, only two phones comparison possible

--- src/SCS.java
+++ src/SCS.java
@@ -1,8 +1,259 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class SCS {
-    public static void main(String[] args) throws FileNotFoundException {
-        Smartphone test = new Smartphone("iPhone13");
-        System.out.println(test);
+    private String directory;
+    List<Smartphone> smartphones;
+
+    private Smartphone firstSelected;
+    private Smartphone secondSelected;
+
+    private int tableCellWidth;
+    private int tableCellHeight;
+
+    public SCS(String directory) {
+        this.directory = directory;
+        updateSmartphoneList();
+        setTableCellParameters(25,4);
+    }
+
+    public void Run() {
+        Scanner scan = new Scanner(System.in);
+        String input;
+        int intInput = 1;
+        boolean parsed = false;
+        int smartphonesSelected = 0;
+
+        while(smartphonesSelected != 2){    //smartphone selection section
+            switch (smartphonesSelected){
+                case 0 -> {                 //first phone selection
+                    printSmartphoneList();
+                    while(!parsed){
+                        try {
+                            System.out.print("Choose first smartphone: ");
+                            input = scan.nextLine();
+                            intInput = Integer.parseInt(input);
+                            parsed = true;
+                            System.out.println("\n\n\n\n\n\n\n\n\n\n\r"); //crappy console clear
+                        } catch (NumberFormatException e) { continue; }
+
+                        if(intInput <= 0 || intInput > smartphones.size() ) { parsed = false; }
+                    }
+                    firstSelected = smartphones.get(intInput - 1);
+
+                    smartphonesSelected++;
+                    parsed = false;
+                }
+                case 1 -> {                 //second phone selection
+                    printSmartphoneList();
+                    while(!parsed){
+                        try {
+                            System.out.print("Choose second smartphone: ");
+                            input = scan.nextLine();
+                            intInput = Integer.parseInt(input);
+                            parsed = true;
+                            System.out.println("\n\n\n\n\n\n\n\n\n\n\r"); //crappy console clear
+                        } catch (NumberFormatException e) { continue; }
+
+                        if(intInput <= 0 || intInput > smartphones.size() ) { parsed = false; }
+                    }
+
+                    if(intInput == smartphones.indexOf(firstSelected) + 1) {    //If user selects the same phone, it will be unselected
+                        firstSelected = null;
+                        smartphonesSelected--;
+                        parsed = false;
+                        continue;
+                    }
+                    secondSelected = smartphones.get(intInput - 1);
+                    smartphonesSelected++;
+                }
+            }
+        }
+
+        printComparisonTable(firstSelected, secondSelected);
+    }
+
+    private void printComparisonTable(Smartphone firstSelected, Smartphone secondSelected) {
+        int smartphones = 2;
+        //ensuring both values are enough to not cause different exceptions while printing
+        ensureTableCellParameters();
+
+        //printing table
+        for (int r = 0; r < 11;r++){
+            for (int h = 0; h < tableCellHeight; h++) {
+                if(h% tableCellHeight == 0){
+                    System.out.print("+");
+                }
+                else {
+                    System.out.print("|");
+                }
+                for(int i = 0; i < smartphones + 1; i++) {
+                    if(h% tableCellHeight == 0){
+                        System.out.print(new String(new char[tableCellWidth -1]).replace("\0", "-") + "+");
+                    }
+                    else if(h == Math.ceil((float) tableCellHeight /2)){
+                        if(i == 0){
+                            switch (r){
+                                case 0 ->
+                                        System.out.print("Name" + new String(new char[tableCellWidth -4-1]).replace("\0", " ") + "|");
+                                case 1 ->
+                                        System.out.print("Brand" + new String(new char[tableCellWidth -5-1]).replace("\0", " ") + "|");
+                                case 2 ->
+                                        System.out.print("Color" + new String(new char[tableCellWidth -5-1]).replace("\0", " ") + "|");
+                                case 3 ->
+                                        System.out.print("Storage" + new String(new char[tableCellWidth -7-1]).replace("\0", " ") + "|");
+                                case 4 ->
+                                        System.out.print("RAM" + new String(new char[tableCellWidth -3-1]).replace("\0", " ") + "|");
+                                case 5 ->
+                                        System.out.print("Battery" + new String(new char[tableCellWidth -7-1]).replace("\0", " ") + "|");
+                                case 6 ->
+                                        System.out.print("Chipset" + new String(new char[tableCellWidth -7-1]).replace("\0", " ") + "|");
+                                case 7 ->
+                                        System.out.print("Resolution" + new String(new char[tableCellWidth -10-1]).replace("\0", " ") + "|");
+                                case 8 ->
+                                        System.out.print("Dimensions" + new String(new char[tableCellWidth -10-1]).replace("\0", " ") + "|");
+                                case 9 ->
+                                        System.out.print("Weight" + new String(new char[tableCellWidth -6-1]).replace("\0", " ") + "|");
+                                case 10 ->
+                                        System.out.print("Release" + new String(new char[tableCellWidth -7-1]).replace("\0", " ") + "|");
+                                default ->
+                                        System.out.print(new String(new char[tableCellWidth -1]).replace("\0", " ") + "|");
+                            }
+                        }
+                        else if(i == 1){
+                            getSpecificSmartphoneInfo(firstSelected, r, tableCellWidth);
+                        }
+                        else {
+                            getSpecificSmartphoneInfo(secondSelected, r, tableCellWidth);
+                        }
+                    }
+                    else{
+                        System.out.print(new String(new char[tableCellWidth -1]).replace("\0", " ") + "|");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        //printing last line
+        System.out.print("+");
+        for(int i = 0; i < smartphones + 1; i++) {
+            System.out.print(new String(new char[tableCellWidth -1]).replace("\0", "-") + "+");
+        }
+    }
+    //this method is created to improve printComparisonSheet readability
+    private void getSpecificSmartphoneInfo(Smartphone selectedSmartphone, int row, int generalWidth) {
+        switch (row){
+            case 0 -> {
+                String content = selectedSmartphone.getName();
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 1 -> {
+                String content = selectedSmartphone.getBrand();
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 2 -> {
+                String content = selectedSmartphone.getColor();
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 3 -> {
+                String content = selectedSmartphone.getStorage().toString() + " GB";
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 4 -> {
+                String content = selectedSmartphone.getRAM().toString() + " GB";
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 5 -> {
+                String content = selectedSmartphone.getBattery().toString() + "mAh";
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 6 -> {
+                String content = selectedSmartphone.getChipset();
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 7 -> {
+                String content = selectedSmartphone.getResolutionX() + "x" + selectedSmartphone.getResolutionY() + "px";
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 8 -> {
+                String content = selectedSmartphone.getWidth() + "x" + selectedSmartphone.getHeight() + "x" + selectedSmartphone.getDepth() + "mm";
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 9 -> {
+                String content = selectedSmartphone.getWeight().toString() + "g";
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            case 10 -> {
+                String content = selectedSmartphone.getReleaseDate().toString();
+                int spaces = generalWidth - content.length();
+                System.out.print(content + new String(new char[spaces-1]).replace("\0", " ") + "|");
+            }
+            default ->
+                    System.out.print(new String(new char[generalWidth-1]).replace("\0", " ") + "|");
+        }
+    }
+
+    public void printSmartphoneList(){
+        List<Smartphone> smartphones = getSmartphoneList(directory);
+        if(smartphones.size() == 0) {
+            System.out.println("No smartphones were found");
+        }
+
+        for (int i = 0; i < smartphones.size(); i++){
+            System.out.print(i+1 + ". " + smartphones.get(i).getName());
+            if(smartphones.get(i).equals(firstSelected) || smartphones.get(i).equals(secondSelected) ){
+                System.out.print("[✓]");
+            }
+            System.out.println();
+        }
+    }
+
+    public List<Smartphone> getSmartphoneList(String directory) {
+        File folder = new File(directory);
+        File[] smartphonePaths = folder.listFiles();
+        List<Smartphone> smartphones = new ArrayList<>();
+
+        assert smartphonePaths != null;
+        for (File path : smartphonePaths) {
+            Smartphone sp;
+            try {
+                sp = new Smartphone(path.getAbsolutePath());
+            }catch (FileNotFoundException e){
+                continue;
+            }
+
+            if(sp.getName() == null) { continue; }
+            smartphones.add(sp);
+        }
+        return smartphones;
+    }
+
+    public void changeDirectory(String directory){
+        this.directory = directory;
+        updateSmartphoneList();
+    }
+    public void setTableCellParameters(int width, int height){
+        tableCellWidth = width;
+        tableCellHeight = height;
+    }
+    private void ensureTableCellParameters(){
+        tableCellWidth = Math.max(tableCellWidth, Math.max(firstSelected.getMaxStringLength(), secondSelected.getMaxStringLength()));
+        tableCellHeight = Math.max(tableCellHeight, 2);
+    }
+    private void updateSmartphoneList(){
+        smartphones = getSmartphoneList(directory);
     }
 }
--- src/Smartphone.java
+++ src/Smartphone.java
@@ -1,27 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Smartphone {
-    String name;
-    String brand;
-    LocalDate releaseDate;
-    Double weight;
-    Integer resolutionX;
-    Integer resolutionY;
-    Double depth;
-    Double width;
-    Double height;
-    Integer storage;
-    Integer RAM;
-    String chipset;
-    String color;
+    private String name;
+    private String brand;
+    private LocalDate releaseDate;
+    private Double weight;
+    private Integer resolutionX;
+    private Integer resolutionY;
+    private Double depth;
+    private Double width;
+    private Double height;
+    private Integer storage;
+    private Integer RAM;
+    private String chipset;
+    private String color;
+    private Integer battery;
 
-    public Smartphone(String smartphone) throws FileNotFoundException
+    public Smartphone(String path) throws FileNotFoundException
     {
-        File parent = new File(System.getProperty("user.dir"));
-        File phoneFile = new File(parent.getAbsolutePath() + "\\Smartphones\\" + smartphone + ".txt");
+        File phoneFile = new File(path);
         Scanner phoneScanner = new Scanner(phoneFile);
         setSpecs(phoneScanner);
     }
@@ -40,6 +41,30 @@ public class Smartphone {
     public Integer getRAM() { return RAM; }
     public String getChipset() { return chipset; }
     public String getColor() { return color; }
+    public Integer getBattery() { return battery; }
+
+
+    public int getMaxStringLength(){
+        int[] lengths = {
+                name.length(),
+                brand.length(),
+                releaseDate.toString().length(),
+                weight.toString().length() + 1, //g
+                resolutionX.toString().length() + resolutionY.toString().length() + 3, //x, px
+                width.toString().length() + height.toString().length() + depth.toString().length() + 4, //x, x, mm
+                storage.toString().length() + 2, //GB
+                RAM.toString().length() + 2, //GB
+                chipset.length(),
+                color.length(),
+                battery.toString().length() + 3 //mAh
+        };
+
+        int maxStringLength = 0;
+        for (int length:lengths) {
+            maxStringLength = Math.max(maxStringLength, length);
+        }
+        return maxStringLength + 1;
+    }
 
     private void setSpecs(Scanner phoneScanner)
     {
@@ -48,6 +73,7 @@ public class Smartphone {
             String line = phoneScanner.nextLine();
             String header = line.substring(0,line.indexOf(':'));
             String content = line.substring(line.indexOf(':') + 1);
+            if(content.equals("")) { continue; }
             switch (header) {
                 case "name" -> name = content;
                 case "brand" -> brand = content;
@@ -66,6 +92,7 @@ public class Smartphone {
                 case "storage" -> storage = Integer.parseInt(content);
                 case "RAM" -> RAM = Integer.parseInt(content);
                 case "color" -> color = content;
+                case "battery" -> battery = Integer.parseInt(content);
             }
         }
     }
@@ -75,15 +102,29 @@ public class Smartphone {
         return  name + "\n" +
                 brand + "\n" +
                 releaseDate + "\n" +
-                weight + "\n" +
-                resolutionX + "\n" +
-                resolutionY + "\n" +
-                height + "\n" +
-                width + "\n" +
-                depth + "\n" +
-                storage + "\n" +
-                RAM + "\n" +
+                weight + "g\n" +
+                resolutionX + "px\n" +
+                resolutionY + "px\n" +
+                height + "mm\n" +
+                width + "mm\n" +
+                depth + "mm\n" +
+                storage + "GB\n" +
+                RAM + "GB\n" +
                 chipset + "\n" +
+                battery + "mAh\n" +
                 color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Smartphone that = (Smartphone) o;
+        return Objects.equals(name, that.name) && Objects.equals(brand, that.brand) && Objects.equals(releaseDate, that.releaseDate) && Objects.equals(weight, that.weight) && Objects.equals(resolutionX, that.resolutionX) && Objects.equals(resolutionY, that.resolutionY) && Objects.equals(depth, that.depth) && Objects.equals(width, that.width) && Objects.equals(height, that.height) && Objects.equals(storage, that.storage) && Objects.equals(RAM, that.RAM) && Objects.equals(chipset, that.chipset) && Objects.equals(color, that.color) && Objects.equals(battery, that.battery);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, brand, releaseDate, weight, resolutionX, resolutionY, depth, width, height, storage, RAM, chipset, color, battery);
     }
 }
